cac66127f28ca40e90dd14437680554302144847
vanpam <vanpamep.q@gmail.com>
2026-04-20 16:21:27 -0400

    corrigiendo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TaskInput from "./componentes/TaskInput";
 import TaskList from "./componentes/TaskList";
 import EmptyState from "./componentes/EmptyState";
 import Footer from "./componentes/Footer";
-import Login from "./componentes/login";
+import Login from "./componentes/Login";
 
 export type Task = {
   id: number;
