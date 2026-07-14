1331a46443902bf920bf8feb30680b069b91719e
Vani <vanpamep.q@gmail.com>
2026-07-14 15:00:37 -0400

    fix: corregir incidente detectado en el pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "./componentes/Header";
 import TaskInput from "./componentes/TaskInput";
-import TaskList from "./componentes/TaskList1";
+import TaskList from "./componentes/TaskList";
 import EmptyState from "./componentes/EmptyState";
 import Footer from "./componentes/Footer";
 import Login from "./componentes/Login";
